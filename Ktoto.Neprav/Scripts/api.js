5ac11df616fe3718c4792d50f6084e0f2f784045
Alexander Makhov <maxobimit@gmail.com>
2014-12-20 14:15:41 +0300

    fix user data loading

--- Ktoto.Neprav/Scripts/api.js
+++ Ktoto.Neprav/Scripts/api.js
@@ -1,4 +1,27 @@
-﻿function getGetArg(name) {
+﻿if (!Array.prototype.find) {
+    Array.prototype.find = function (predicate) {
+        if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
+
+function getGetArg(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
         results = regex.exec(location.search);
@@ -13,7 +36,9 @@ loadUsersIntoDivs = function (authorsToLoad) {
         if (data.response) {
             authorsToLoad.each(function (ix, div) {
                 var authorControl = $(div);
-                var userData = data.response[ix];
+                var userData = data.response.find(function(el) {
+                    return el.uid == authorControl.data('user-id');
+                });
                 var avatarControl = authorControl.find('.avatar');
                 avatarControl.attr('src', userData.photo_100);
                 var name = userData.first_name + ' ' + userData.last_name;
